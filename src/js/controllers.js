679f8245d7cea06e5c17442ec180e22b0dff0d75
Max Mathys <Max.Mathys@swisscom.com>
2016-09-11 15:59:04 +0200

    This is not the commit message you are looking for

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,7 +1,12 @@
-angular.module('app', ['ngMaterial'])
+//import db from 'nedb';
+//ws
+(function() {
+  'use strict';
+  angular.module('app', ['ngMaterial', 'electangular'])
 
-.controller('HomeController', function($scope) {
+  .controller('HomeController', function($scope, electron) {
 
-  $scope.test = "sadfasdfasdf";
 
-});
+
+  });
+})();
